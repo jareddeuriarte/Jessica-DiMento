8ddc5e21861b95a4c4e43f8f440eff00da7de029
Jared de Uriarte <jareddeuriarte@Jareds-MacBook-Pro-2.local>
2023-11-04 17:34:48 -0400

    fine carousel

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const carousel = document.querySelector(".carousel");
-  const slides = document.querySelectorAll(".carousel-slide");
   const prevButton = document.querySelector(".prev");
   const nextButton = document.querySelector(".next");
-  const slidesToShow = 3; // Change this to 4 to display 4 images at a time
   let currentIndex = 0;
 
   nextButton.addEventListener("click", () => {
-    if (currentIndex < slides.length - slidesToShow) {
+    if (currentIndex < carousel.children.length - 3) {
       currentIndex++;
       updateCarousel();
     } else {
@@ -21,12 +19,82 @@ document.addEventListener("DOMContentLoaded", function () {
       currentIndex--;
       updateCarousel();
     } else {
-      currentIndex = slides.length - slidesToShow;
+      currentIndex = carousel.children.length - 3;
       updateCarousel();
     }
   });
 
   function updateCarousel() {
-    carousel.style.transform = `translateX(-${currentIndex * (100 / slides.length)}%)`;
+    carousel.style.transform = `translateX(-${currentIndex * (100 / carousel.children.length)}%)`;
   }
+});
+
+
+
+const carouselImages = document.querySelectorAll(".carousel-slide img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImage = document.getElementById("lightbox-image");
+const lightboxClose = document.getElementById("lightbox-close");
+
+carouselImages.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    lightboxImage.src = image.src;
+    lightbox.style.display = "block";
+  });
+});
+
+lightboxClose.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".carousel");
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  const lightbox = document.getElementById("lightbox");
+  const lightboxImage = document.getElementById("lightbox-image");
+  const lightboxClose = document.getElementById("lightbox-close");
+
+  let currentIndex = 0;
+
+  nextButton.addEventListener("click", () => {
+    if (currentIndex < carousel.children.length - 4) {
+      currentIndex++;
+      updateCarousel();
+    } else {
+      currentIndex = 0;
+      updateCarousel();
+    }
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      updateCarousel();
+    } else {
+      currentIndex = carousel.children.length - 4;
+      updateCarousel();
+    }
+  });
+
+  function updateCarousel() {
+    carousel.style.transform = `translateX(-${currentIndex * (100 / carousel.children.length)}%)`;
+  }
+
+
+
+  
+  const carouselImages = document.querySelectorAll(".carousel-slide img");
+
+  carouselImages.forEach((image, index) => {
+    image.addEventListener("click", () => {
+      lightboxImage.src = image.src;
+      lightbox.style.display = "block";
+    });
+  });
+
+  lightboxClose.addEventListener("click", () => {
+    lightbox.style.display = "none";
+  });
 });
