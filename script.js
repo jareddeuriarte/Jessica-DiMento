857b5e74be7c882ace1b515a84f63af0435361d3
Jared de Uriarte <jareddeuriarte@jareds-mbp-2.mynetworksettings.com>
2023-11-21 21:58:14 -0500

    testing menus

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function toggleDropdown() {
     dropdown.style.display = "block";
   }
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const accordions = document.querySelectorAll(".accordion-btn");
+
+  accordions.forEach(function(accordion) {
+    accordion.addEventListener("click", function() {
+      this.classList.toggle("active");
+      const panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  });
+});
